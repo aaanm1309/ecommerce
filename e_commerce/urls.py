7f790efd8f1b772166f2cd8c88f90c57b063ba79
aaanm1309 <aaanm1309@gmail.com>
2020-02-09 23:56:58 -0300

    1.0

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from e_commerce import views
 
 urlpatterns = [
+    path('', views.home_page,name='homepage'),
     path('admin/', admin.site.urls),
 ]
